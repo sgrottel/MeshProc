bc30a6a69070bb7d418243d90f0490b46953bf23
SGrottel <sgrottel@sgrottel.de>
2025-10-04 06:18:37 +0200

    [WIP] MorphRotateOutline requires parameter-space coordinates

--- MeshProc/fitting/MorphRotateOutline.cpp
+++ MeshProc/fitting/MorphRotateOutline.cpp
@@ -20,17 +20,167 @@ using namespace meshproc;
 
 namespace
 {
+
+	// MAX exclusive
+	template <typename T, T MIN, T NEUTRAL, T MAX>
+	class CyclicFloat
+	{
+		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type");
+		static_assert(MIN <= NEUTRAL, "MIN must be less or equal NEUTRAL");
+		static_assert(NEUTRAL <= MAX, "NEUTRAL must be less or equal MAX");
+		static_assert(MIN < MAX, "MIN must be less MAX");
+
+		constexpr static const T LEN = MAX - MIN;
+
+	public:
+		CyclicFloat() : m_value{ NEUTRAL } {}
+		CyclicFloat(T value)
+			: m_value{ NEUTRAL }
+		{
+			(*this) = value;
+		}
+		~CyclicFloat() = default;
+		CyclicFloat(CyclicFloat const& src) : m_value{ src.m_value } {}
+		CyclicFloat(CyclicFloat&& src) : m_value{ src.m_value } {}
+		CyclicFloat& operator=(CyclicFloat const& src)
+		{
+			m_value = src.m_value;
+			return *this;
+		}
+		CyclicFloat& operator=(CyclicFloat&& src)
+		{
+			m_value = src.m_value;
+			return *this;
+		}
+
+		CyclicFloat& operator=(T value)
+		{
+			if (MIN <= value && value <= MAX)
+			{
+				m_value = value;
+			}
+			else
+			{
+				m_value = MIN + std::fmod(value - MIN, LEN);
+			}
+			return *this;
+		}
+
+		inline T GetValue() const noexcept
+		{
+			return m_value;
+		}
+
+		//  a - b
+		T Minus(CyclicFloat const& b) const
+		{
+			const T d = m_value - b;
+			if (m_value >= b)
+			{
+				assert(d >= 0 && d <= LEN);
+				if (d > LEN / 2)
+				{
+					return d - LEN;
+				}
+			}
+			else
+			{
+				assert(d <= 0 && d >= -LEN);
+				if (d < LEN / 2)
+				{
+					return d + LEN;
+				}
+			}
+			return d;
+		}
+
+		CyclicFloat& operator+(T b)
+		{
+			return (*this) = (m_value + b);
+		}
+
+	private:
+		T m_value;
+	};
+
+	template <typename T, T MIN, T NEUTRAL, T MAX>
+	class ClampedFloat
+	{
+		static_assert(std::is_floating_point<T>::value, "T must be a floating-point type");
+		static_assert(MIN <= NEUTRAL, "MIN must be less or equal NEUTRAL");
+		static_assert(NEUTRAL <= MAX, "NEUTRAL must be less or equal MAX");
+		static_assert(MIN < MAX, "MIN must be less MAX");
+
+		constexpr static const T LEN = MAX - MIN;
+
+	public:
+		ClampedFloat() : m_value{ NEUTRAL } {}
+		ClampedFloat(T value)
+			: m_value{ NEUTRAL }
+		{
+			(*this) = value;
+		}
+		~ClampedFloat() = default;
+		ClampedFloat(ClampedFloat const& src) : m_value{ src.m_value } {}
+		ClampedFloat(ClampedFloat&& src) : m_value{ src.m_value } {}
+		ClampedFloat& operator=(ClampedFloat const& src)
+		{
+			m_value = src.m_value;
+			return *this;
+		}
+		ClampedFloat& operator=(ClampedFloat&& src)
+		{
+			m_value = src.m_value;
+			return *this;
+		}
+
+		ClampedFloat& operator=(T value)
+		{
+			if (value < MIN)
+			{
+				m_value = MIN;
+			}
+			else if (value > MAX)
+			{
+				m_value = MAX;
+			}
+			else
+			{
+				m_value = value;
+			}
+			return *this;
+		}
+
+		inline operator T() const noexcept
+		{
+			return m_value;
+		}
+
+	private:
+		T m_value;
+	};
+
+	struct MapCoord
+	{
+		static constexpr double pi = std::numbers::pi_v<double>;
+
+		CyclicFloat<double, -pi, 0.0, pi> m_primeAng;
+		ClampedFloat<double, -pi / 2, 0.0, pi / 2> m_secAng;
+		double m_length;
+		CyclicFloat<double, 0.0, 0.0, 1.0> m_param;
+	};
+
 	class CoordSys
 	{
 	public:
-		CoordSys(glm::vec3 const& o, glm::vec3 const& z)
+		CoordSys(glm::vec3 const& o, glm::vec3 const& z, std::vector<glm::vec3> const& border)
 			: m_origin{ o }
 			, m_x { 0, 0, 0 }
 			, m_y{ 0, 0, 0 }
 			, m_z{ z }
 		{
-			assert(std::abs(glm::length(m_z) - 1.0f) < 0.0001f);
-			constexpr const glm::vec3 gx{ 1.0, 0.0, 0.0 };
+			assert(std::abs(glm::length(m_z) - 1.0) < 0.0001);
+			constexpr const glm::dvec3 gx{ 1.0, 0.0, 0.0 };
 
 			if (std::abs(glm::dot(m_z, gx)) < 0.99)
 			{
@@ -40,140 +190,100 @@ namespace
 			else
 			{
 				// m_z ~== gx
-				m_x = glm::vec3{ 0.0f, 0.0f, 1.0f };
+				m_x = glm::dvec3{ 0.0, 0.0, 1.0 };
 			}
 			m_y = glm::normalize(glm::cross(m_z, m_x));
 			m_x = glm::normalize(glm::cross(m_y, m_z));
+
+			assert(border.size() > 2);
+			m_border.resize(border.size());
+			std::copy(border.begin(), border.end(), m_border.begin());
+
+			m_borderDataSet = std::move(flann::Matrix<double>(
+				glm::value_ptr(m_border.front()),
+				m_border.size(),
+				3));
+			m_borderIndex = std::move(flann::Index<flann::L2<double>>(m_borderDataSet, flann::KDTreeIndexParams(1)));
+			m_borderIndex.buildIndex();
+
 		}
 
-		glm::vec3 const& Origin() const noexcept
+		MapCoord Map(glm::vec3 const& v) const
 		{
-			return m_origin;
-		}
-		glm::vec3 const& X() const noexcept
-		{
-			return m_x;
-		}
-		glm::vec3 const& Y() const noexcept
-		{
-			return m_y;
-		}
-		glm::vec3 const& Z() const noexcept
-		{
-			return m_z;
-		}
+			const glm::dvec3 r = glm::dvec3{ v } - m_origin;
 
-	private:
-		glm::vec3 m_origin;
-		glm::vec3 m_x;
-		glm::vec3 m_y;
-		glm::vec3 m_z;
-	};
+			const double rl = glm::length(r);
 
-	// a - b wraparound at 180°|pi
-	// output range: ]-180°|-pi .. 180°|pi]
-	template <typename T>
-	typename std::enable_if_t<std::is_floating_point_v<T>, T>
-	AngleDiff(T a, T b)
-	{
-		constexpr T pi = std::numbers::pi_v<T>;
-		const T v = std::fmod(a - b, 2 * pi); // v is [-2pi..2pi]
-		if (v > pi)
-		{
-			return v - 2 * pi;
-		}
-		if (v <= -pi)
-		{
-			return v + 2 * pi;
-		}
-		return v;
-	}
+			const glm::dvec3 c = glm::normalize(
+				glm::dvec3{
+					glm::dot(m_x, r),
+					glm::dot(m_y, r),
+					glm::dot(m_z, r) });
+			const glm::dvec2 c2 = glm::normalize(glm::dvec2{ c.x, c.y });
 
-	class RollCoord
-	{
-	public:
-		using ftype = double;
-		using Vec3Type = glm::dvec3;
-		using Vec2Type = glm::dvec2;
-		using DiffT = glm::dvec2;
+			const double roll = std::atan2(c2.y, c2.x);
+			const double pitch = std::asin(c.z);
 
-	private:
-		static constexpr double pi = std::numbers::pi_v<ftype>;
+			// TODO: Impl param space value
 
-	public:
-		RollCoord() = default;
-		RollCoord(const RollCoord& src) = default;
-		RollCoord(RollCoord&& src) = default;
-		RollCoord& operator=(const RollCoord& src) = default;
-		RollCoord& operator=(RollCoord&& src) = default;
-
-		explicit RollCoord(glm::vec3 const& v, CoordSys const& sys)
-		{
-			const Vec3Type r = v - sys.Origin();
-			const ftype rl = glm::length(r);
-			const Vec3Type c = glm::normalize(
-				Vec3Type{
-					glm::dot(static_cast<Vec3Type>(sys.X()), r),
-					glm::dot(static_cast<Vec3Type>(sys.Y()), r),
-					glm::dot(static_cast<Vec3Type>(sys.Z()), r)
-				});
-			const Vec2Type c2 = glm::normalize(Vec2Type{ c.x, c.y });
-
-			m_roll = std::atan2(c2.y, c2.x);
-			m_pitch = std::asin(c.z);
-			m_dist = rl;
-
-			const glm::vec3 recon = RollCoord::ToVec3(sys);
-			const glm::vec3 diff = recon - v;
-			const float diffVal = glm::length(diff);
-			assert(diffVal < 0.0001f);
-		}
-
-		glm::vec3 ToVec3(CoordSys const& sys) const
-		{
-			const Vec3Type c = glm::normalize(
-				Vec3Type{
-					std::cos(m_roll) * std::cos(m_pitch),
-					std::sin(m_roll) * std::cos(m_pitch),
-					std::sin(m_pitch)
-				}) * m_dist;
-
-			return sys.Origin()
-				+ sys.X() * static_cast<float>(c.x)
-				+ sys.Y() * static_cast<float>(c.y)
-				+ sys.Z() * static_cast<float>(c.z);
-		}
-
-		DiffT CalcRollTo(RollCoord const& rhs) const
-		{
-			return {
-				AngleDiff(rhs.m_roll, m_roll),
-				rhs.m_pitch - m_pitch
+			const MapCoord mc{
+				.m_primeAng = roll,
+				.m_secAng = pitch,
+				.m_length = rl,
+				.m_param = 0.0
 			};
+#if _DEBUG
+			const glm::vec3 v2 = Unmap(mc);
+			const float diffVal = glm::distance(v, v2);
+			assert(diffVal >= 0 && diffVal < 0.0001f);
+#endif
+			return mc;
 		}
 
-		ftype PrimaryAngle() const
+		glm::vec3 Unmap(MapCoord const& m) const
 		{
-			return m_roll;
+			const glm::dvec3 c = glm::normalize(
+				glm::dvec3{
+					std::cos(m.m_primeAng.GetValue()) * std::cos(m.m_secAng),
+					std::sin(m.m_primeAng.GetValue()) * std::cos(m.m_secAng),
+					std::sin(m.m_secAng)
+				}) * m.m_length;
+
+			const glm::dvec3 v = m_origin
+				+ m_x * c.x
+				+ m_y * c.y
+				+ m_z * c.z;
+			return glm::vec3{ v };
 		}
 
-		ftype Length() const
-		{
-			return m_dist;
-		}
-
-		void ApplyRoll(DiffT const & roll)
-		{
-			m_roll = AngleDiff(m_roll + roll.x, 0.0);
-			m_pitch = std::clamp(m_pitch + roll.y, -pi, pi);
-		}
+		//glm::vec3 const& Origin() const noexcept
+		//{
+		//	return m_origin;
+		//}
+		//glm::vec3 const& X() const noexcept
+		//{
+		//	return m_x;
+		//}
+		//glm::vec3 const& Y() const noexcept
+		//{
+		//	return m_y;
+		//}
+		//glm::vec3 const& Z() const noexcept
+		//{
+		//	return m_z;
+		//}
 
 	private:
-		ftype m_roll;	// in XY plane ]-180°..180°]
-		ftype m_pitch;	// pitch towards z [-90°..90°]
-		ftype m_dist;
+		glm::dvec3 m_origin;
+		glm::dvec3 m_x;
+		glm::dvec3 m_y;
+		glm::dvec3 m_z;
+		std::vector<glm::dvec3> m_border;
+		flann::Matrix<double> m_borderDataSet{};
+		flann::Index<flann::L2<double>> m_borderIndex{ flann::KDTreeIndexParams(1) };
 	};
 
+	/*
 	class SmoothRoll
 	{
 	private:
@@ -277,7 +387,7 @@ namespace
 		}
 
 	};
-
+	*/
 }
 
 struct fitting::MorphRotateOutline::Impl
@@ -295,10 +405,11 @@ struct fitting::MorphRotateOutline::Impl
 		targetIndex.buildIndex();
 	}
 
-	CoordSys csys{ glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 0, 1 } };
+	CoordSys csys{ glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 0, 1 }, { glm::vec3{}, glm::vec3{}, glm::vec3{} } };
 
-	void BuildCoordinateSystem(const glm::vec3& center, const glm::vec3& primaryAxis)
+	void BuildCoordinateSystem(const glm::vec3& center, const glm::vec3& primaryAxis, const std::shared_ptr<data::Mesh> mesh, const std::vector<uint32_t>& border)
 	{
+		(border); // todo compute better unfolding space
 		const glm::vec3 axisRoll = glm::normalize(primaryAxis);
 		{
 			const float len = glm::length(axisRoll);
@@ -307,7 +418,11 @@ struct fitting::MorphRotateOutline::Impl
 				throw std::runtime_error("PrimaryAxis normalization failed");
 			}
 		}
-		csys = std::move(CoordSys{ center, axisRoll });
+		
+		std::vector<glm::vec3> b{ border.size() };
+		std::transform(border.begin(), border.end(), b.begin(), [&mesh](uint32_t i) { return mesh->vertices.at(i); });
+
+		csys = std::move(CoordSys{ center, axisRoll, b });
 	}
 
 	std::vector<uint32_t> border;
@@ -365,14 +480,16 @@ bool fitting::MorphRotateOutline::Invoke()
 
 	m_impl = std::make_shared<Impl>();
 	m_impl->BuildTargetIndex(m_targetMesh);
-	m_impl->BuildCoordinateSystem(m_center, m_primaryAxis);
 	m_impl->SetBorder(GetMeshBorderVertices());
+	m_impl->BuildCoordinateSystem(m_center, m_primaryAxis, m_mesh, m_impl->border);
 
-	SmoothRotateIterate(90, 6, 0.2); // 0.2
-	SmoothRotateIterate(90, 5, 0.25); // 0.2 + (0.8 * 0.25) = 0.4
-	SmoothRotateIterate(90, 4, 0.333); // 0.4 + (0.6 * 0.33) ~= 0.6
-	SmoothRotateIterate(90, 3, 0.5); // 0.6 + (0.4 * 0.5) = 0.8
-	SmoothRotateIterate(90, 2, 1);
+	//SmoothRotateIterate(90, 6, 0.2); // 0.2
+	//SmoothRotateIterate(90, 5, 0.25); // 0.2 + (0.8 * 0.25) = 0.4
+	//SmoothRotateIterate(90, 4, 0.333); // 0.4 + (0.6 * 0.33) ~= 0.6
+	//SmoothRotateIterate(90, 3, 0.5); // 0.6 + (0.4 * 0.5) = 0.8
+	//SmoothRotateIterate(90, 2, 1);
+
+	SmoothRotateIterate(90, 5, 1);
 
 	m_impl.reset();
 	return false;
@@ -383,7 +500,7 @@ void fitting::MorphRotateOutline::SmoothRotateIterate(size_t numBuckets, double 
 	assert(m_impl != nullptr);
 	Log().Detail("SmoothRotateIterate(%d, %f, %f)", numBuckets, smooth, factor);
 
-	std::vector<RollCoord> queryCoords(m_impl->borderSize);
+	std::vector<MapCoord> queryCoords(m_impl->borderSize);
 
 	// prepare query
 	std::vector<float> query_flat(m_impl->borderSize * 3);
@@ -391,13 +508,11 @@ void fitting::MorphRotateOutline::SmoothRotateIterate(size_t numBuckets, double 
 	for (size_t i = 0; i < m_impl->borderSize; ++i)
 	{
 		glm::vec3 const& v1 = m_mesh->vertices.at(m_impl->border.at(i));
-		queryCoords.at(i) = RollCoord{ v1, m_impl->csys };
+		queryCoords.at(i) = m_impl->csys.Map(v1);
 
-		glm::vec3 v2 = queryCoords.at(i).ToVec3(m_impl->csys);
-
-		query_flat.at(i * 3 + 0) = v2.x;
-		query_flat.at(i * 3 + 1) = v2.y;
-		query_flat.at(i * 3 + 2) = v2.z;
+		query_flat.at(i * 3 + 0) = v1.x;
+		query_flat.at(i * 3 + 1) = v1.y;
+		query_flat.at(i * 3 + 2) = v1.z;
 	}
 
 	// Allocate result buffers
@@ -406,33 +521,33 @@ void fitting::MorphRotateOutline::SmoothRotateIterate(size_t numBuckets, double 
 	flann::Matrix<int> indices(indices_vec.data(), m_impl->borderSize, 1);
 	flann::Matrix<float> dists(dists_vec.data(), m_impl->borderSize, 1);
 
-	std::vector<RollCoord::DiffT> rollVal(m_impl->borderSize);
+	// std::vector<RollCoord::DiffT> rollVal(m_impl->borderSize);
 
 	m_impl->targetIndex.knnSearch(query, indices, dists, 1, flann::SearchParams(128));
 
-	for (size_t i = 0; i < m_impl->borderSize; ++i)
-	{
-		RollCoord target{ m_targetMesh->vertices.at(indices_vec.at(i)), m_impl->csys };
-		rollVal.at(i) = queryCoords.at(i).CalcRollTo(target);
-	}
-
-	SmoothRoll smoothRoll;
-	smoothRoll.Init(numBuckets);
-	smoothRoll.Build(rollVal, queryCoords, smooth);
-
-	for (size_t i = 0; i < m_impl->borderSize; ++i)
-	{
-		auto roll = smoothRoll.Query(queryCoords.at(i).PrimaryAngle());
-		queryCoords.at(i).ApplyRoll(roll * factor);
-	}
-
-	// TODO: Implement
-
-	for (size_t i = 0; i < m_impl->borderSize; ++i)
-	{
-		m_mesh->vertices.at(m_impl->border.at(i)) = queryCoords.at(i).ToVec3(m_impl->csys);
-		//m_mesh->vertices.at(border.at(i)) = m_targetMesh->vertices.at(indices_vec.at(i));
-	}
+	// for (size_t i = 0; i < m_impl->borderSize; ++i)
+	// {
+	// 	RollCoord target = m_impl->csys.VecToRoll(m_targetMesh->vertices.at(indices_vec.at(i)));
+	// 	rollVal.at(i) = queryCoords.at(i).CalcRollTo(target);
+	// }
+	// 
+	// SmoothRoll smoothRoll;
+	// smoothRoll.Init(numBuckets);
+	// smoothRoll.Build(rollVal, queryCoords, smooth);
+	// 
+	// for (size_t i = 0; i < m_impl->borderSize; ++i)
+	// {
+	// 	auto roll = smoothRoll.Query(queryCoords.at(i).PrimaryAngle());
+	// 	queryCoords.at(i).ApplyRoll(roll * factor);
+	// }
+	// 
+	// // TODO: Implement
+	// 
+	// for (size_t i = 0; i < m_impl->borderSize; ++i)
+	// {
+	// 	m_mesh->vertices.at(m_impl->border.at(i)) = m_impl->csys.RollToVec(queryCoords.at(i));
+	// 	//m_mesh->vertices.at(border.at(i)) = m_targetMesh->vertices.at(indices_vec.at(i));
+	// }
 }
 
 std::vector<uint32_t> fitting::MorphRotateOutline::GetMeshBorderVertices() const
