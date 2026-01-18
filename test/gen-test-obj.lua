b1003c65ba149a016eb3c75400320653853cd382
SGrottel <sgrottel@sgrottel.de>
2026-01-18 14:12:01 +0100

    Added test/test-commands-1.lua based on current demo.lua
    Including an error to test CI execution

--- test/gen-test-obj.lua
+++ test/gen-test-obj.lua
@@ -5,30 +5,30 @@
 -- Run with:
 --   MeshProc.exe run $(SolutionDir)test\gen-test-obj.lua -v
 --
-meshprocVersion.assertOrNewer(0, 5, 0)
-meshprocVersion.assertOlderThan(0, 6, 0)
+meshproc.Version.assert_or_newer(0, 5, 0)
+meshproc.Version.assert_older_than(0, 6, 0)
 
 local xyz_math = require("xyz_math")
 
 local make = meshproc.generator.Cuboid.new()
 
-make:set("SizeX", 2)
-make:set("SizeY", 4)
-make:set("SizeZ", 6)
+make["SizeX"] = 2
+make["SizeY"] = 4
+make["SizeZ"] = 6
 make:invoke()
-local cube1 = make:get("Mesh")
+local cube1 = make["Mesh"]
 
-make:set("SizeX", 2)
-make:set("SizeY", 1)
-make:set("SizeZ", 0.5)
+make["SizeX"] = 2
+make["SizeY"] = 1
+make["SizeZ"] = 0.5
 make:invoke()
-local cube2 = make:get("Mesh")
+local cube2 = make["Mesh"]
 
 local scene = meshproc.Scene.new()
 scene:place(cube1, XMat4.translate(-1, -2, -3))
 scene:place(cube2, XMat4.translate(3, -0.5, -0.25))
 
 local file = meshproc.io.ObjWriter.new()
-file:set("Scene", scene)
-file:set("Path", "test-obj.obj")
+file["Scene"] = scene
+file["Path"] = "test-obj.obj"
 file:invoke()
---
+++ test/run-all.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+param(
+	[Parameter(Mandatory = $true)][string]$exe
+)
+
+# Forward common parameters if present
+$innerParams = @{
+	exe = $exe
+}
+foreach ($key in 'Verbose','Debug','ErrorAction','WarningAction') {
+	if ($PSBoundParameters.ContainsKey($key)) {
+		$innerParams[$key] = $PSBoundParameters[$key]
+	}
+}
+
+Write-Host "run-test-obj.ps1" -Foreground Cyan -Background Black
+& (Join-Path $PSScriptRoot "run-test-obj.ps1") -exe:$exe @innerParams
+
+Write-Host "run-test-commands-1.ps1" -Foreground Cyan -Background Black
+& (Join-Path $PSScriptRoot "run-test-commands-1.ps1") -exe:$exe @innerParams
+
+Write-Host "done." -Foreground Cyan -Background Black
