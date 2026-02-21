d513a2cc27241ca9a10eb5ec5b7a3de7ae6997b1
SGrottel <sgrottel@sgrottel.de>
2026-02-21 19:23:22 +0100

    "demo.lua" now uses "test-bars.stl" for smaller testing

--- MeshProc/demo.lua
+++ MeshProc/demo.lua
@@ -15,42 +15,54 @@ math.randomseed(1236)
 local xyz_math = require("xyz_math")
 log.write("Test script")
 
-local mesh = nil
+local mesh0 = nil
+
+-- do
+-- 	local reader = meshproc.io.PlyReader.new()
+-- 	reader.Path = "../test/test-open-torus.ply"
+-- 	reader:invoke()
+-- 	mesh0 = reader.Mesh
+-- end
+-- do
+-- 	local scene = meshproc.Scene.new()
+-- 	scene:place(mesh0)
+-- 	scene:place(mesh0, XMat4.translate(3, 0, -3) * XMat4.rotation_y(math.rad(180)) * XMat4.rotation_z(math.rad(90)) * XMat4.translate(-3, 0, 0))
+-- 	scene:place(mesh0, XMat4.translate(-10, 0, 0))
+-- 	mesh0 = scene:bake()
+-- end
+
 do
-	local reader = meshproc.io.PlyReader.new()
-	reader.Path = "../test/test-open-torus.ply"
+	local reader = meshproc.io.StlReader.new()
+	reader.Path = "../test/test-bars.stl"
 	reader:invoke()
-	mesh = reader.Mesh
+	mesh0 = reader.Mesh
 end
 
-do
-	local scene = meshproc.Scene.new()
-	scene:place(mesh)
-	scene:place(mesh, XMat4.translate(3, 0, -3) * XMat4.rotation_y(math.rad(180)) * XMat4.rotation_z(math.rad(90)) * XMat4.translate(-3, 0, 0))
-	scene:place(mesh, XMat4.translate(-10, 0, 0))
-	mesh = scene:bake()
-end
-
-local mesh2 = nil
+local mesh1 = nil
 do
 	local p = meshproc.compute.ProjectionScarf.new()
-	p.Mesh = mesh
+	p.Mesh = mesh0
+
 	local pp = meshproc.HalfSpace.new()
-	pp:set(XVec3(1, 0.2, 0), XVec3(0, 0, 0))
+
+	--pp:set(XVec3(1, 0.2, 0), XVec3(0, 0, 0))
+
+	pp:set(XVec3(0, 0, 1), 0)
+
 	p.Projection = pp
 	p:invoke()
-	mesh2 = p.OutMesh
+	mesh1 = p.OutMesh
 end
 
 -- save scene to file
 do
 	local file = meshproc.io.ObjWriter.new()
 
-	file.Scene = mesh
+	file.Scene = mesh1
 	file.Path = "out.obj"
 	file:invoke()
 
-	file.Scene = mesh2
-	file.Path = "out2.obj"
-	file:invoke()
+	-- file.Scene = mesh0
+	-- file.Path = "out0.obj"
+	-- file:invoke()
 end
