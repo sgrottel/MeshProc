a3dd03c8a7bb620cb447be2d28191c95de28e208
SGrottel <sgrottel@sgrottel.de>
2026-03-15 08:45:53 +0100

    restarting ProjectionScarf implementation yet again

--- MeshProc/commands/compute/ProjectionScarf.h
+++ MeshProc/commands/compute/ProjectionScarf.h
@@ -23,9 +23,15 @@ namespace meshproc
 				bool Invoke() override;
 
 			private:
+				struct Mesh2D;
+
+				std::vector<uint32_t> SelectTriangles();
+				Mesh2D ProjectTriangles(std::vector<uint32_t> const& selTris, glm::vec3 const& x2d, glm::vec3 const& y2d);
+
 				const std::shared_ptr<data::Mesh> m_mesh;
 				const std::shared_ptr<data::HalfSpace> m_projection;
 				std::shared_ptr<data::Mesh> m_outmesh;
+
 			};
 
 		}
