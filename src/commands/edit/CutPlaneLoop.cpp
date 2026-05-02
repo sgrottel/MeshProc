4ef68a4a90f38f89060e39cf27be63bf219c1709
SGrottel <sgrottel@sgrottel.de>
2026-05-02 11:21:29 +0200

    Selecting the right loop to cut

--- src/commands/edit/CutPlaneLoop.cpp
+++ src/commands/edit/CutPlaneLoop.cpp
@@ -1,6 +1,6 @@
 #include "CutPlaneLoop.h"
 
-//#include "utilities/LoopsFromEdges.h"
+#include "utilities/LoopsFromEdges.h"
 
 #include <SimpleLog/SimpleLog.hpp>
 
@@ -20,13 +20,39 @@
 //
 //#include <algorithm>
 //#include <functional>
-//#include <unordered_map>
-//#include <unordered_set>
+#include <unordered_map>
+#include <unordered_set>
 //#include <vector>
 
 using namespace meshproc;
 using namespace meshproc::commands;
 using namespace meshproc::commands::edit;
+
+namespace
+{
+	float EdgeDist(const data::HashableEdge& edge, const glm::vec3& pt, const std::vector<glm::vec3>& vertices)
+	{
+		const glm::vec3& v0 = vertices.at(edge.i0);
+		const glm::vec3& v1 = vertices.at(edge.i1);
+
+		const glm::vec3 ve = v1 - v0;
+		const glm::vec3 r = pt - v0;
+
+		const float edgeLen2 = glm::dot(ve, ve);
+		const float a = glm::dot(r, ve) / edgeLen2;
+
+		if (a < 0.0)
+		{
+			return glm::distance(v0, pt);
+		}
+		if (a > 1.0)
+		{
+			return glm::distance(v1, pt);
+		}
+
+		return glm::distance(v0 + r * a, pt);
+	}
+}
 
 CutPlaneLoop::CutPlaneLoop(const sgrottel::ISimpleLog& log)
 	: AbstractCommand{ log }
@@ -66,22 +92,213 @@ bool CutPlaneLoop::Invoke()
 	);
 
 	// first select all triangles touching the plane
+	// compute edges on plane, adding new vertices to 'm_mesh' (we'll clean up later)
 	std::vector<uint32_t> tris;
+	std::unordered_set<data::HashableEdge> edges;
+	std::unordered_map<data::HashableEdge, uint32_t> newVert;
 	for (uint32_t ti = 0; ti < static_cast<uint32_t>(m_mesh->triangles.size()); ++ti)
 	{
-		const float d0 = dist.at(m_mesh->triangles.at(ti)[0]);
-		const float d1 = dist.at(m_mesh->triangles.at(ti)[1]);
-		const float d2 = dist.at(m_mesh->triangles.at(ti)[2]);
+		const auto& t = m_mesh->triangles.at(ti);
+		const float d[3] = { dist.at(t[0]), dist.at(t[1]), dist.at(t[2]) };
+		const bool hasNeg = d[0] < 0.0f || d[1] < 0.0f || d[2] < 0.0f;
+		const bool hasNul = d[0] == 0.0f || d[1] == 0.0f || d[2] == 0.0f;
+		const bool hasPos = d[0] > 0.0f || d[1] > 0.0f || d[2] > 0.0f;
 
-		const bool hasNeg = d0 < 0.0f || d1 < 0.0f || d2 < 0.0f;
-		const bool hasNul = d0 == 0.0f || d1 == 0.0f || d2 == 0.0f;
-		const bool hasPos = d0 > 0.0f || d1 > 0.0f || d2 > 0.0f;
-
-		if (hasNul || (hasNeg && hasPos))
+		if (!hasNul && !(hasNeg && hasPos))
 		{
-			tris.push_back(ti);
+			continue;
+		}
+		tris.push_back(ti);
+
+		if (hasNeg && hasPos)
+		{
+			// tri is cut
+			uint32_t v0 = std::numeric_limits<uint32_t>::max(), v1 = std::numeric_limits<uint32_t>::max();
+			for (uint32_t i = 0; i < 3; ++i)
+			{
+				const uint32_t j = (i + 1) % 3;
+				if ((d[i] < 0.0f && d[j] > 0.0f) || (d[i] > 0.0f && d[j] < 0.0f))
+				{
+					// edge is cut
+					data::HashableEdge e{ t[i], t[j] };
+					if (!newVert.contains(e))
+					{
+						glm::vec3 v = m_plane->CutInterpolate(e, dist, m_mesh->vertices);
+						newVert.insert({ e, static_cast<uint32_t>(m_mesh->vertices.size()) });
+						m_mesh->vertices.push_back(v);
+					}
+					uint32_t vi = newVert.at(e);
+					if (v0 == std::numeric_limits<uint32_t>::max())
+					{
+						v0 = vi;
+					}
+					else
+					{
+						assert(v1 == std::numeric_limits<uint32_t>::max());
+						v1 = vi;
+					}
+
+				}
+				else if (d[i] == 0)
+				{
+					// vertex on plane
+					if (v0 == std::numeric_limits<uint32_t>::max())
+					{
+						v0 = t[i];
+					}
+					else
+					{
+						assert(v1 == std::numeric_limits<uint32_t>::max());
+						v1 = t[i];
+					}
+				}
+			}
+
+			assert(v1 != std::numeric_limits<uint32_t>::max());
+			edges.insert(data::HashableEdge{ v0, v1 });
+		}
+		else
+		{
+			assert(hasNul);
+			if (d[0] == 0)
+			{
+				if (d[1] == 0)
+				{
+					edges.insert(data::HashableEdge{ t[0], t[1] });
+				}
+				else if (d[2] == 0)
+				{
+					edges.insert(data::HashableEdge{ t[0], t[2] });
+				}
+			}
+			else if (d[1] == 0 && d[2] == 0)
+			{
+				edges.insert(data::HashableEdge{ t[1], t[2] });
+			}
+			// all other cases mean only one vertex is on the plane, aka the loop is formed by neighboring triangles only.
 		}
 	}
+
+	// The loop to cut!
+	std::vector<uint32_t> loop;
+	{
+		// collect all loops on the plane
+		std::shared_ptr<std::vector<std::shared_ptr<std::vector<uint32_t>>>> openLoops;
+		utilities::LoopsFromEdges(edges, openLoops, Log());
+
+		auto [projX, projY] = m_plane->Make2DCoordSys();
+		float minX = 0.0f;
+		std::unordered_map<uint32_t, glm::vec2> pt2d;
+		for (auto loop : *openLoops)
+		{
+			for (uint32_t vi : *loop)
+			{
+				if (pt2d.contains(vi)) continue;
+				const glm::vec3 v = m_mesh->vertices.at(vi) - m_plane->Plane();
+				const float x = glm::dot(v, projX);
+				pt2d.insert(std::make_pair(vi, glm::vec2(x, glm::dot(v, projY))));
+				if (x < minX)
+				{
+					minX = x;
+				}
+			}
+		}
+
+		if (openLoops->size() < 1)
+		{
+			Log().Error("No open loops extracted");
+			return false;
+		}
+		else if (openLoops->size() == 1)
+		{
+			std::swap(loop, *openLoops->front());
+		}
+		else
+		{
+			assert(openLoops->size() > 1);
+			// indentify selected loop
+
+			float minLoopDist = std::numeric_limits<float>::max();
+			size_t minLoopIdx = 0;
+			for (size_t loopI = 0; loopI < openLoops->size(); ++loopI)
+			{
+				const auto& loop = *openLoops->at(loopI);
+				float loopDist = std::numeric_limits<float>::max();
+				uint32_t pIdx = loop.back();
+				for (uint32_t idx : loop)
+				{
+					data::HashableEdge e{pIdx, idx};
+					pIdx = idx;
+					const float dist = EdgeDist(e, m_point, m_mesh->vertices);
+					if (dist < loopDist)
+					{
+						loopDist = dist;
+					}
+				}
+				if (minLoopDist > loopDist)
+				{
+					minLoopDist = loopDist;
+					minLoopIdx = loopI;
+				}
+			}
+
+			std::swap(loop, *openLoops->at(minLoopIdx));
+		}
+
+		// only keep triangles in "tris" that are in the selected `loop`
+		{
+			std::unordered_set<uint32_t> loopVerts;
+			loopVerts.reserve(loop.size());
+			for (uint32_t vi : loop)
+			{
+				loopVerts.insert(vi);
+			}
+
+			std::vector<uint32_t> keepTris;
+			for (uint32_t ti : tris)
+			{
+				const auto& t = m_mesh->triangles.at(ti);
+				bool keep = false;
+				for (uint32_t i = 0; i < 3; ++i)
+				{
+					if (loopVerts.contains(t[i]))
+					{
+						keep = true;
+						break;
+					}
+					auto hei = newVert.find(t.HashableEdge(i));
+					if (hei != newVert.end())
+					{
+						if (loopVerts.contains(hei->second))
+						{
+							keep = true;
+							break;
+						}
+					}
+				}
+				if (keep)
+				{
+					keepTris.push_back(ti);
+				}
+			}
+			std::swap(keepTris, tris);
+		}
+	}
+
+	// duplicate the loop for the neg size
+	std::vector<uint32_t> loopNeg;
+	loopNeg.reserve(loop.size());
+	m_mesh->vertices.reserve(m_mesh->vertices.size() + loop.size());
+	for (uint32_t vi : loop)
+	{
+		loopNeg.push_back(static_cast<uint32_t>(m_mesh->vertices.size()));
+		m_mesh->vertices.push_back(m_mesh->vertices.at(vi));
+	}
+
+
+
+	// TODO
+
 
 	std::erase_if(m_mesh->triangles,
 		[&](const data::Triangle& t)
@@ -100,6 +317,7 @@ bool CutPlaneLoop::Invoke()
 			return false;
 		});
 
+	m_mesh->RemoveIsolatedVertices();
 
 	return false;
 }
--- test/test-cutplaneloop.lua
+++ test/test-cutplaneloop.lua
@@ -4,55 +4,70 @@ meshproc.Version.assert_older_than(0, 7, 0)
 log.detail("MeshProc v"..tostring(meshproc.Version.get()).." ["..tostring(meshproc.Version.get()[1]).."."..tostring(meshproc.Version.get()[2]).."]")
 local xyz_math = require("xyz_math")
 
-local mesh = nil
+local mesh1 = nil
+local mesh2 = nil
 do -- load
 	local reader = meshproc.io.StlReader.new()
 	reader.Path = "test-torus.stl"
 	reader:invoke()
-	mesh = reader.Mesh
+	mesh1 = reader.Mesh
+	mesh2 = mesh1:clone()
 end
 
 
 do -- cut
 	local cut = meshproc.edit.CutPlaneLoop.new()
-	cut.Mesh = mesh
+	cut.Mesh = mesh1
 	cut.Plane = meshproc.HalfSpace.new()
 
 	cut.Point = XVec3(-2.8, 2.8, 0) -- point
 	cut.Plane:set( XVec3(-1, -1, 0.01), cut.Point) -- normal
 
 	cut:invoke()
+
+	cut.Mesh = mesh2
+	cut.Plane:set( XVec3(-1, -1, 0), cut.Point) -- normal
+	cut:invoke()
 end
 
 
-local meshval = nil
+local mesh1col = nil
+local mesh2col = nil
 do
 	local vedtc = meshproc.compute.VertexEdgeDistanceToCut.new()
-	vedtc.Mesh = mesh
+	local colmap = meshproc.compute.LinearColorMap.new()
+
+	vedtc.Mesh = mesh1
 	vedtc.PlaneOrigin = XVec3(-3.5, 0, 0)
 	vedtc.PlaneNormal = XVec3(-1, -0.1, 0)
 	vedtc.PlaneXAxis = XVec3(0, 1, 0)
 	vedtc.PlaneRectWidth = 10
 	vedtc.PlaneRectHeight = 10
 	vedtc:invoke()
-	meshval = vedtc.Distances
-end
-
-local meshcol = nil
-do
-	local colmap = meshproc.compute.LinearColorMap.new()
-	colmap.Scalars = meshval
+	colmap.Scalars = vedtc.Distances
 	colmap:invoke()
-	meshcol = colmap.Colors
+	mesh1col = colmap.Colors
+
+	vedtc.Mesh = mesh2
+	vedtc:invoke()
+	colmap.Scalars = vedtc.Distances
+	colmap:invoke()
+	mesh2col = colmap.Colors
+
 end
 
 -- save scene to file
 do
+	local scene = meshproc.Scene.new()
+	scene:place(mesh1)
+	scene:place(mesh2, XMat4.translate(10, 0, 0))
+
 	local scenecol = meshproc.Vec3ListList.new()
-	scenecol:insert(meshcol)
+	scenecol:insert(mesh1col)
+	scenecol:insert(mesh2col)
 
 	local file = meshproc.io.ObjWriter.new()
-	file.Scene = mesh
+	file.Scene = scene
 	file.VertexColors = scenecol
 	file.Path = "test-cutplaneloop.obj"
 	file:invoke()
